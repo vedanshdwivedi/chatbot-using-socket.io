c9761ff35ee11f95348783b8e2f81bf550aa41b1
vedansh <Vedansh@quadrical.ai>
2022-02-13 02:43:41 +0530

    Setup Express and Dependable

--- container.js
+++ container.js
@@ -4,10 +4,19 @@ const dependable = require("dependable");
 const path = require("path");
 
 const container = dependable.container();
-const simpleDependencies = [["_", "lodash"]];
+const simpleDependencies = [["_", "lodash"], ['async', 'async']];
 
 simpleDependencies.forEach(function (val) {
   container.register(val[0], function () {
     return require(val[1]);
   });
 });
+
+container.load(path.join(__dirname, "/controllers"));
+container.load(path.join(__dirname, "/helpers"));
+
+container.register("container", function () {
+  return container;
+});
+
+module.exports = container;
